aece31672de09f08b3a1399c8515268d51b5466e
bernarddwyerb <bernarddwyerb@gmail.com>
2021-02-19 16:08:03 -0500

    fin exercie 2

--- src/composants/Dossier.jsx
+++ src/composants/Dossier.jsx
@@ -1,15 +1,22 @@
+import './Dossier.scss'
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SortIcon from '@material-ui/icons/Sort';
+
 export default function Dossier({id, titre, couleur, dateModif}) {
+  let couleurBackground = {
+    backgroundColor: couleur
+  }
   return (
     <article className="Dossier">
       <div className="couverture">
-        <span className="deplacer">Déplacer</span>
+        <span className="deplacer"><SortIcon/></span>
         <img src={`images-dossiers/${id}.png`} alt={titre}/>
       </div>
-      <div className="info">
+      <div className="info" style={couleurBackground}>
         <h2>{titre}</h2>
         <p>{dateModif}</p>
       </div>
-      <button>Modifier</button>
+      <button><MoreVertIcon /></button> 
     </article>
   );
 }
--- src/composants/Entete.jsx
+++ src/composants/Entete.jsx
@@ -1,10 +1,11 @@
-import avatar from '../images/avatar.png';
+import './Entete.scss'
+import avatar from '../images/avatar.jpg';
 
 export default function Entete() {
   return (
     <header className="Entete">
       <div className="logo">Signets</div>
-      <div className="avatar">Camille Semaan <img src={avatar} alt="Camille Semaan" /></div>
+      <div className="avatar">Bernard Dwyer<img src={avatar} alt="Camille Semaan" /></div>
     </header>
   );
 }
